c72b607466b31f2d5eac7550c9f2e0dbe8a2d451
Bamidelejulius12 <babatundejulius911@gmail.com>
2026-03-30 20:53:49 +0100

    folder structure updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a project that comprises of all datasceince infrastructure',
+    author='bamidele julius',
+    license='MIT',
+)
